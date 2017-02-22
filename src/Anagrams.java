584c6e81895987d32d4a0506c66f83625df1463b
chriscarrillo <christopherscott.carrillo@calbaptist.edu>
2017-02-22 15:28:43 -0800

    HW4 Part 2 Commit

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -1,16 +1,38 @@
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Chris on 2/9/17.
  */
 public class Anagrams {
-    public Anagrams(List<String> dictionary) {
+    private Map<String, LetterInventory> solver;
+    private List<String> dictionary;
 
+    public Anagrams(List<String> dictionary) {
+        this.dictionary = dictionary;
+        solver = new HashMap<String, LetterInventory>();
+        for (String word : dictionary) {
+            solver.put(word, new LetterInventory(word));
+        }
     }
 
     public void print(String text, int max) throws IllegalArgumentException {
-        if (max < 0) {
+        if (max < 0)
             throw new IllegalArgumentException("Max cannot be less than zero.");
+        print(max, new Stack<String>(), new LetterInventory(text));
+    }
+
+    private void print(int max, Stack<String> stack, LetterInventory inventory) {
+        if (inventory.isEmpty()) {
+            System.out.println(stack);
+        } else if (stack.size() < max || max == 0) {
+            for(String word : this.dictionary){
+                LetterInventory copy = inventory.subtract(this.solver.get(word));
+                if(copy != null){
+                    stack.push(word);
+                    print(max, stack, copy);
+                    stack.pop();
+                }
+            }
         }
     }
 }
